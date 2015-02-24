cc05d790473509ff7c1e4e7b1c2236ea2331d715
Anjana Tissera <anjanat@gmail.com>
2015-02-24 23:33:49 +0700

    Initial commit of ZoneMinder connector plugin

---
+++ ZoneMinderConnector/src/VmsClient.hpp
@@ -0,0 +1,20 @@
+/*
+ * VmsClient.hpp
+ *
+ *  Created on: Feb 24, 2015
+ *      Author: anjana
+ */
+
+#ifndef VMSCLIENT_HPP_
+#define VMSCLIENT_HPP_
+
+class VmsClient
+{
+public:
+	VmsClient(){}
+	virtual ~VmsClient(){}
+};
+
+
+
+#endif /* VMSCLIENT_HPP_ */
