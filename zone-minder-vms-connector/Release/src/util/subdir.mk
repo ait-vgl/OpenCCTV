0b483ad12b8cc6d9514dfe643fda51149f544134
Nhutumdai <sornrak.kp@gmail.com>
2016-07-20 17:39:58 +0700

    V 1

---
+++ zone-minder-vms-connector/Release/src/util/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../src/util/Config.cpp 
+
+OBJS += \
+./src/util/Config.o 
+
+CPP_DEPS += \
+./src/util/Config.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/util/%.o: ../src/util/%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -O3 -Wall -c -fmessage-length=0 -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
