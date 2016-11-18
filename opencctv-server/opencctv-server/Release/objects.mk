b50ef82f630ee48f616a66a60b7db9300589614e
Nhutumdai <sornrak.kp@gmail.com>
2016-11-18 18:03:29 +0700

    Remove some config makefile

--- opencctv-server/opencctv-server/Release/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -lboost_thread -lboost_system -lboost_program_options -lboost_filesystem -lprotobuf -lzmq -ldl -lmysqlcppconn
-
